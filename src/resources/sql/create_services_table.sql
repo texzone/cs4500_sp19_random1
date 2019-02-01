702850fab38aa3696db17d608ba790575383a47a
Tyler Rivenburgh <rivenburgh.t@husky.neu.edu>
2019-01-31 20:15:06 -0500

    Sorry, changing back to code provided

--- src/resources/sql/create_services_table.sql
+++ src/resources/sql/create_services_table.sql
@@ -1,7 +1,6 @@
-
-CREATE TABLE `services` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `service_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
+DROP TABLE IF EXISTS services;
+CREATE TABLE services (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  service_name varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
