0233bd756f6d3d63e01d95f10e1eccb97de00350
tanvis <sehgal.t@husky.neu.edu>
2019-02-07 15:35:25 -0500

    added frequently asked question class and repo

---
+++ src/main/java/com/example/cs4500_sp19_random1/models/FrequentlyAskedQuestion.java
@@ -0,0 +1,59 @@
+package com.example.cs4500_sp19_random1.models;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="frequently_asked_questions")
+public class FrequentlyAskedQuestion {
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private Integer id;
+  private String title;
+  private String question;
+  @OneToMany(mappedBy="frequentlyAskedQuestion")
+  private List<FrequentlyAskedAnswer> answers;
+  public List<FrequentlyAskedAnswer> getAnswers() {
+    return answers;
+  }
+  public void setAnswers(List<FrequentlyAskedAnswer> answers) {
+    this.answers = answers;
+  }
+  public Integer getId() {
+    return id;
+  }
+  public void setId(Integer id) {
+    this.id = id;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public String getQuestion() {
+    return question;
+  }
+  public void setQuestion(String question) {
+    this.question = question;
+  }
+  public void answeredQuestion(FrequentlyAskedAnswer answer) {
+    this.answers.add(answer);
+    if (answer.getQuestion() != this.question) {
+      answer.setQuestion(this.question);
+    }
+  }
+
+    public void removeAnswer(FrequentlyAskedAnswer answer) {
+    if (this.answers.contains(answer)) {
+      this.answers.remove(answer);
+      }
+    }
+  }
+
---
+++ src/main/java/com/example/cs4500_sp19_random1/repositories/FrequentlyAskedQuestionRepository.java
@@ -0,0 +1,19 @@
+package com.example.cs4500_sp19_random1.repositories;
+import com.example.cs4500_sp19_random1.models.FrequentlyAskedQuestion;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface FrequentlyAskedQuestionRepository extends
+        CrudRepository<FrequentlyAskedQuestion, Integer> {
+
+  @Query(value="SELECT frequentQuestions FROM FrequentlyAskedQuestion frequentQuestions")
+  public List<FrequentlyAskedQuestion> findAllFrequentQuestions();
+  @Query(value="SELECT frequentQuestions FROM FrequentlyAskedQuestion frequentQuestions WHERE frequentQuestions.id=:id")
+  public FrequentlyAskedQuestion findFAQById(@Param("id") Integer id);
+
+}
+
