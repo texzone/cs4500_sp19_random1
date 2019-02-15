f3f3369432e273609d0a2dd78706f1e8e226372b
texzone <ali.elgabri@gmail.com>
2019-02-14 19:40:03 -0500

    service category fix

--- src/main/java/com/example/cs4500_sp19_random1/models/ServiceCategory.java
+++ src/main/java/com/example/cs4500_sp19_random1/models/ServiceCategory.java
@@ -8,20 +8,32 @@ import javax.persistence.*;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.List;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name="service_categories")
 public class ServiceCategory {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
-    private String serviceCategoryName;
+    private String title;
     @ManyToMany
     @JsonIgnore
-    @JoinTable(name="CATEGORIES_SERVICES",
-               joinColumns=@JoinColumn(name="CATEGORY_ID", 
-                                       referencedColumnName="ID"),
-               inverseJoinColumns=@JoinColumn(name="SERVICE_ID", 
-                                              referencedColumnName="ID"))
+    @JoinTable(
+            name="CATEGORIES_SERVICES",
+            joinColumns=@JoinColumn(name="CATEGORY_ID", referencedColumnName="ID"),
+            inverseJoinColumns=@JoinColumn(name="SERVICE_ID", referencedColumnName="ID"))
     private List<Service> services;
     public Integer getId() {
         return id;
@@ -29,17 +41,15 @@ public class ServiceCategory {
     public void setId(Integer id) {
         this.id = id;
     }
-    public String getServiceCategoryName() {
-        return serviceCategoryName;
+    public String getTitle() {
+        return title;
     }
-    public void setServiceCategoryName(String serviceCategoryName) {
-        this.serviceCategoryName = serviceCategoryName;
+    public void setTitle(String title) {
+        this.title = title;
     }
-
     public List<Service> getServices() {
         return services;
     }
-
     public void setServices(List<Service> services) {
         this.services = services;
     }
