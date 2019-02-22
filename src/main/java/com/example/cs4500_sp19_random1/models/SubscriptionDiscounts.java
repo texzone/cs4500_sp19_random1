75c00e4ffd18cad3b7998ef1465523292b9cafa0
Nicholas Buquicchio <nbuquicchio@carbonite.com>
2019-02-22 13:55:10 -0500

    Interface map to concreate class hashmap

--- src/main/java/com/example/cs4500_sp19_random1/models/SubscriptionDiscounts.java
+++ src/main/java/com/example/cs4500_sp19_random1/models/SubscriptionDiscounts.java
@@ -3,12 +3,15 @@ package com.example.cs4500_sp19_random1.models;
 import java.util.*;
 
 public class SubscriptionDiscounts {
-    private Map<Frequency, Float> discountMap;
+    private HashMap<Frequency, Float> discountMap;
     private boolean isFlat;
 
-    public SubscriptionDiscounts() {}
+    public SubscriptionDiscounts() {
+        this.discountMap = new HashMap<Frequency, Float>();
+        this.isFlat      = false;
+    }
     
-    public SubscriptionDiscounts(Map<Frequency, Float> discMap, boolean flat) {
+    public SubscriptionDiscounts(HashMap<Frequency, Float> discMap, boolean flat) {
         this.discountMap = discMap;
         this.isFlat      = flat;
     }
@@ -23,7 +26,7 @@ public class SubscriptionDiscounts {
         }
     }
 
-    public void setDiscountMap(Map<Frequency, Float> discMap) {
+    public void setDiscountMap(HashMap<Frequency, Float> discMap) {
         this.discountMap = discMap;
     }
 
