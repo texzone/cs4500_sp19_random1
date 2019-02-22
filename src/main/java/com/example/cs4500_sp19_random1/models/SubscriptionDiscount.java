1fbb0207aa8a54334309f2af8b0d5a764c43efa4
Nicholas Buquicchio <nbuquicchio@carbonite.com>
2019-02-22 13:19:38 -0500

    Implementation for SubscriptionDiscount

---
+++ src/main/java/com/example/cs4500_sp19_random1/models/SubscriptionDiscount.java
@@ -0,0 +1,37 @@
+package com.example.cs4500_sp19_random1.models;
+
+import java.util.*;
+
+public class SubscriptionDiscount {
+    private Map<Frequency, Float> discountMap;
+    private boolean isFlat;
+
+    public SubscriptionDiscount() {}
+    
+    public SubscriptionDiscount(Map<Frequency, Float> discMap, boolean flat) {
+        this.discountMap = discMap;
+        this.isFlat      = flat;
+    }
+
+    public float getDiscountFor(Frequency f) {
+        Float discount = this.discountMap.get(f);
+        if (discount == null) {
+            return 0f;
+        }
+        else {
+            return discount.floatValue();
+        }
+    }
+
+    public void setDiscountMap(Map<Frequency, Float> discMap) {
+        this.discountMap = discMap;
+    }
+
+    public boolean getIsFlat() {
+        return this.isFlat;
+    }
+
+    public void setIsFlat(boolean isFlat) {
+        this.isFlat = isFlat;
+    }
+}
