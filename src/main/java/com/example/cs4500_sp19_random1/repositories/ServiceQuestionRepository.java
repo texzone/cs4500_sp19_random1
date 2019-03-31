9f81b04466bbc5197fc9e1c1fbe1460b7c650a7c
Abdullah <alowain.a@husky.neu.edu>
2019-03-31 16:25:01 -0400

    Added service and repo

---
+++ src/main/java/com/example/cs4500_sp19_random1/repositories/ServiceQuestionRepository.java
@@ -0,0 +1,11 @@
+package com.example.cs4500_sp19_random1.repositories;
+
+import com.example.cs4500_sp19_random1.models.ServiceQuestion;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ServiceQuestionRepository extends CrudRepository<ServiceQuestion, Integer> {
+
+
+
+}
