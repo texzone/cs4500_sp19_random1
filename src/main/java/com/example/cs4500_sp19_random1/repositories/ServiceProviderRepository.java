0d060822402228adb1d140eea5d2a36c8fd1bec4
Tyler Rivenburgh <rivenburgh.t@husky.neu.edu>
2019-03-31 23:09:48 -0400

    init working commit

--- src/main/java/com/example/cs4500_sp19_random1/repositories/ServiceProviderRepository.java
+++ src/main/java/com/example/cs4500_sp19_random1/repositories/ServiceProviderRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import com.example.cs4500_sp19_random1.models.FrequentlyAskedQuestion;
 import com.example.cs4500_sp19_random1.models.ServiceProvider;
 
 public interface ServiceProviderRepository extends CrudRepository<ServiceProvider, Integer> {
@@ -13,6 +14,10 @@ public interface ServiceProviderRepository extends CrudRepository<ServiceProvide
   public List<ServiceProvider> findAllServiceProviders();
   @Query(value="SELECT provider FROM ServiceProvider provider WHERE provider.id=:id")
   public ServiceProvider findServiceProviderById(@Param("id") Integer id);
-  @Query(value="SELECT provider FROM ServiceProvider provider WHERE provider.username=:username")
+  @Query(value="SELECT provider FROM ServiceProvider provider WHERE provider.name=:username")
   public ServiceProvider findByServiceProvidername(@Param("username") String username);
+  @Query(value="SELECT provider FROM ServiceProvider provider WHERE (:provider is null or provider.name LIKE :provider%) AND " +
+          "(:zipCode is null or provider.zipCode=:zipCode)")
+  public List<ServiceProvider> filterAllServiceProviders(@Param("provider") String provider,
+                                                                  @Param("zipCode") String zipCode);
 }
