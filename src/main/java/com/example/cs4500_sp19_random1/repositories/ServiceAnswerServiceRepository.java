a891bb233de05f9522011c0e6e0e6203ae3d5ae9
Abdullah <alowain.a@husky.neu.edu>
2019-04-05 01:04:19 -0400

    fixed ServiceAnswerServiceRepo

--- src/main/java/com/example/cs4500_sp19_random1/repositories/ServiceAnswerServiceRepository.java
+++ src/main/java/com/example/cs4500_sp19_random1/repositories/ServiceAnswerServiceRepository.java
@@ -1,14 +1,15 @@
 package com.example.cs4500_sp19_random1.repositories;
 
 import com.example.cs4500_sp19_random1.models.ServiceAnswer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ServiceAnswerServiceRepository extends CrudRepository<ServiceAnswer, Integer> {
-
+    @Query(value="SELECT serviceAnswer FROM ServiceAnswer serviceAnswer")
     List<ServiceAnswer> findAllServiceAnswers();
-
-    ServiceAnswer findServiceAnswerById(Integer qid);
-
+    @Query(value="SELECT serviceAnswer FROM ServiceAnswer serviceAnswer WHERE serviceAnswer.id=:id")
+    ServiceAnswer findServiceAnswerById(@Param("id") Integer id);
 }
