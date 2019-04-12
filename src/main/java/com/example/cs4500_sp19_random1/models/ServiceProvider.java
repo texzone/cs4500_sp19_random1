fbf7af4dfed86edd05ce2a5c0dd8e5c354d6c2ee
Abdullah <alowain.a@husky.neu.edu>
2019-04-12 05:13:22 -0400

    Added classes for provider info + test

--- src/main/java/com/example/cs4500_sp19_random1/models/ServiceProvider.java
+++ src/main/java/com/example/cs4500_sp19_random1/models/ServiceProvider.java
@@ -1,53 +1,24 @@
 package com.example.cs4500_sp19_random1.models;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.OneToMany;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 
 
 @Entity
-@Table(name = "service_providers")
-public class ServiceProvider {
-  @Id
-  @GeneratedValue(strategy= GenerationType.IDENTITY)
-  private Integer id;
-  @OneToMany(mappedBy = "providers")
-  private List<Service> services;
-  private String name;
-  private String zipCode;
+public class ServiceProvider extends Provider {
 
-  public void setId(Integer id) {
-    this.id = id;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JsonIgnore
+  private Service service;
+
+  public Service getService() {
+    return service;
   }
 
-  public Integer getId() {
-    return this.id;
-  }
-
-  public String getName() {return this.name;}
-
-  public String getZipCode() {
-    return zipCode;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setZipCode(String zipCode) {
-    this.zipCode = zipCode;
-  }
-
-  public List<Service> getServices() {
-    return this.services;
-  }
-
-  public void setServices(List<Service> services) {
-    this.services = services;
+  public void setService(Service service) {
+    this.service = service;
   }
 }
